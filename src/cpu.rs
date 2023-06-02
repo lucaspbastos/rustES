c0f7be9a26d3849c7bf014ef4d0875455ec86e30
Lucas Bastos <lucas@lucasbastos.com>
2023-06-01 21:49:14 -0400

    Set up addressing modes and some instructions

--- src/cpu.rs
+++ src/cpu.rs
@@ -8,6 +8,23 @@ pub struct CPU {
     pub ps: u8,
 }
 
+enum AddressingModes {
+    Implicit,
+    Accumulator,
+    Immediate,
+    ZeroPage,
+    ZeroPageX,
+    ZeroPageY,
+    Relative,
+    Absolute,
+    AbsoluteX,
+    AbsoluteY,
+    IndirectX,
+    IndirectY,
+    IndexedIndirect,
+    IndirectIndexed,
+}
+
 fn get_nth_bit_u8(byte: u8, n: u8) -> u8 {
     return (byte >> n) & 1;
 }
@@ -26,6 +43,13 @@ impl CPU {
             y: 0,
             ps: 0,
         };
+    }
+
+    //TODO
+    fn handle_addressing_mode(&self, mode: &AddressingModes) {
+        // match {
+        //     _ => println!("Unknown");
+        // }
     }
 
     fn get_carry_flag(&mut self) -> u8 {
@@ -116,6 +140,67 @@ impl CPU {
         self.pc += 1;
     }
 
+    fn update_zero_and_negative_flags_u8(&mut self, bit: u8) {
+        if bit == 0 {
+            self.set_zero_flag();
+        } else {
+            self.unset_zero_flag();
+        }
+        if get_nth_bit_u8(bit, 7) == 1 {
+            self.set_negative_flag();
+        } else {
+            self.unset_negative_flag();
+        }
+    }
+
+    fn cpy(&mut self, val: u8) {
+        let result = self.y - val;
+
+        if self.y >= val {
+            self.set_carry_flag();
+        } else {
+            self.unset_carry_flag();
+        }
+
+        if self.y == val {
+            self.set_zero_flag()
+        } else {
+            self.unset_zero_flag()
+        }
+
+        if get_nth_bit_u8(result, 7) == 1 {
+            self.set_negative_flag();
+        } else {
+            self.unset_negative_flag();
+        }
+    }
+
+    fn inx(&mut self) {
+        self.x = self.x.wrapping_add(1);
+
+        if self.x == 0 {
+            self.set_zero_flag();
+        } else {
+            self.unset_zero_flag();
+        }
+
+        if get_nth_bit_u8(self.x, 7) == 1 {
+            self.set_negative_flag();
+        } else {
+            self.unset_negative_flag();
+        }
+    }
+
+    fn lda(&mut self, val: u8) {
+        self.a = val;
+        self.update_zero_and_negative_flags_u8(self.a);
+    }
+
+    fn tax(&mut self) {
+        self.x = self.a;
+        self.update_zero_and_negative_flags_u8(self.x);
+    }
+
     pub fn interpret(&mut self, program: Vec<u8>) {
         self.pc = 0;
         loop {
@@ -126,7 +211,29 @@ impl CPU {
             match opcode {
                 0x00 => {
                     return;
-                },
+                }
+                //LDA: immediate
+                0xA9 => {
+                    let param = program[self.pc as usize];
+                    self.increment_program_counter();
+
+                    self.lda(param);
+                }
+                //TAX: implicit
+                0xAA => {
+                    self.tax();
+                }
+                //CPY: immediate
+                0xC0 => {
+                    let param = program[self.pc as usize];
+                    self.increment_program_counter();
+
+                    self.cpy(param);
+                }
+                //INX: immediate
+                0xe8 => {
+                    self.inx();
+                }
                 _ => println!("Unknown"),
             }
         }
